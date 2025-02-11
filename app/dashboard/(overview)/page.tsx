cf72c653d8290adcbdbfec8997fc170770987290
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-02-11 17:36:00 +0100

    ✨ feat: adding invoice paging

--- app/dashboard/(overview)/page.tsx
+++ app/dashboard/(overview)/page.tsx
@@ -6,7 +6,7 @@ import CardWrapper from '@/app/ui/dashboard/cards';
 import { 
   RevenueChartSkeleton, 
   LatestInvoicesSkeleton,
-  CardSkeleton,
+  CardsSkeleton,
 } from '@/app/ui/skeletons';
 
 export default async function Page() {
@@ -16,7 +16,7 @@ export default async function Page() {
         Dashboard
       </h1>
       <div className="grid gap-6 sm:grid-cols-2 lg:grid-cols-4">
-        <Suspense fallback={<CardSkeleton />}>
+        <Suspense fallback={<CardsSkeleton />}>
           <CardWrapper />
         </Suspense>
       </div>
