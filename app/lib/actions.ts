442746fc61fc4fc28992890eb2aa651732ec393f
Nathanaël Derousseaux <n.derousseaux@icloud.com>
2025-02-12 16:11:15 +0100

    🐛 fix: typo from -> form

--- app/lib/actions.ts
+++ app/lib/actions.ts
@@ -20,7 +20,7 @@ const UpdateInvoice = FormSchema.omit({ id: true, date: true });
  
 
 
-export async function createInvoice(formData: FromData) {
+export async function createInvoice(formData: FormData) {
 	const { customerId, amount, status } = CreateInvoice.parse({
     customerId: formData.get('customerId'),
     amount: formData.get('amount'),
